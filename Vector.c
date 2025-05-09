b9630ae68017ad6f5a1c92234d560d733354dd8e
tudorfiiiase <floreatudor23@stud.ase.ro>
2025-05-09 19:09:30 +0300

    Add files via upload

---
+++ Vector.c
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#define CRT_SECURE_NO_WARNINGS
+#define MAXN 1000000
+
+int A[MAXN];
+
+int main() {
+    int n, k;
+    scanf_s("%d %d", &n, &k);
+
+    for (int i = 0; i < n; i++) {
+        scanf_s("%d", &A[i]);
+    }
+
+    long long sum = 0, maxSum;
+
+    // Suma initiala pe prima fereastra
+    for (int i = 0; i < k; i++)
+        sum += A[i];
+    maxSum = sum;
+
+    // Glisam fereastra de la stanga la dreapta
+    for (int i = k; i < n; i++) {
+        sum = sum - A[i - k] + A[i];
+        if (sum > maxSum)
+            maxSum = sum;
+    }
+
+    printf("%lld\n", maxSum);
+    return 0;
+}
+
